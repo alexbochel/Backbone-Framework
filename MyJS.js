6f2dfa5b4d541b989a88ff29682910fdc4194089
Alexander James Bochel <alexander.bochel@excella.com>
2018-04-18 12:51:46 -0400

    Added a model and a view for 'sandwich'.

--- MyJS.js
+++ MyJS.js
@@ -1,9 +1,32 @@
-WebsiteModel = Backbone.Model.extend({
+SandwichModel = Backbone.Model.extend({
+    defaults: {
+        bread: "wheat"
+    },
+
     initialize: function() {
-        alert("Hi father");
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        this.on("change:bread", function(model) {
+            let new_bread = model.get("bread");
+            alert("The bread changed to -> " + new_bread);
+      });
+    }
+});
+
+SandwichView = Backbone.View.extend({
+    tagName: "p",
+    className: "paragraph",
+    id: "bread-paragraph",
+
+    initialize: function() {
+        $("#main-div").append(this.el);
     }
 });
 
 $(document).ready(function() {
-    let page_model = new WebsiteModel();
+    let first_sandwich = new SandwichModel();
+    let sandwich_view = new SandwichView();
+    first_sandwich.set({bread: "pumpernickle"});
 });
